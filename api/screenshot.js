6120941697b4b97a9c03afd320c88b78c9cd0ce0
Tabea Reiffer <tr@cyon.ch>
2022-10-20 15:01:32 +0200

    initial commit

---
+++ api/screenshot.js
@@ -0,0 +1,16 @@
+import { chromium } from "playwright-core";
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        const {domain} = req.query;
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
+        await page.goto('https://' + domain);
+        const img = await page.screenshot();
+        await browser.close();
+        res.setHeader('content-type', 'image/png')
+        return res.send(img);
+    } else {
+        return res.send('Post Request not allowed');
+    }
+  }
